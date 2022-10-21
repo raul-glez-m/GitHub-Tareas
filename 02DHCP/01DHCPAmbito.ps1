45916a9e51574afac27d857c410c2b5b95c2e26e
Raul <rgonzalezm01@educantabria.es>
2022-10-21 17:36:20 +0200

    cambios

--- 02DHCP/01DHCPAmbito.ps1
+++ 02DHCP/01DHCPAmbito.ps1
@@ -7,5 +7,10 @@
 
 Remove-DhcpServerv4Scope -ScopeId 172.20.140.192 -force
 Add-DhcpServerv4Scope -EndRange 172.20.140.195 -Name AmbitosPowershell -StartRange 172.20.140.193 -SubnetMask 255.255.255.192 -LeaseDuration 8:30:00
+
+# Añadir opciones "dominio DNS", "DNS Server" y "Router"
 Set-DhcpServerv4OptionValue -DnsDomain domi.com -DnsServer 172.20.140.254, 8.8.8.8 -Router 172.20.140.254 -ScopeId 172.20.140.192 
+
+# Añadir Server TFTP
+Set-DhcpServerv4OptionValue -OptionId 64 -Value 172.20.140.192 -ScopeId 172.20.140.192
 
