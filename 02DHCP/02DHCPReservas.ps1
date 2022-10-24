53e7f2bae8bd9e059987aa70fcf6f18900463469
Raul <rgonzalezm01@educantabria.es>
2022-10-24 15:29:49 +0200

    Final

--- 02DHCP/02DHCPReservas.ps1
+++ 02DHCP/02DHCPReservas.ps1
@@ -1,10 +1,11 @@
-﻿$MiIP = 196
-$DatosMac = Import-CSV -Path C:\Users\Administrador\Desktop\02DHCPReservas_csv.csv -Delimiter ';'
-Foreach ($fila in $DatosMac) {
+﻿$DatosMac = Import-CSV -Path C:\Users\Administrador\Desktop\02DHCPReservas_csv.csv -Delimiter ';'
+$MiIP = 196
+foreach ($fila in $DatosMac) {
     Write-Host ($fila.'MAC Address')
-    Add-DhcpServerv4Reservation -ScopeId 172.20.140.192 -ClientId $fila.'MAC Address' -IPAddress -172.20.140.$MiIP ++
+    Add-DhcpServerv4Reservation -ScopeId 172.20.140.192 -ClientId $fila.'MAC Address' -IPAddress 172.20.140.$MiIP
+$MiIP ++
 }
 
-Write-Host $DatosMac
+# Write-Host $DatosMac
 
-Add-DhcpServerv4Reservation -ScopeId 172.20.140.192 -ClientId $fila.'MAC Address' -IPAddress -172.20.140.$MiIP
+# Add-DhcpServerv4Reservation -ScopeId 172.20.140.192 -ClientId $fila.'MAC Address' -IPAddress 172.20.140.$MiIP
